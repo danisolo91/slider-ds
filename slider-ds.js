4d9ccc56a2e7d9e0c028e08ae2bef742ea9a135c
Daniel <danisolo91@gmail.com>
2021-02-23 21:53:48 +0100

    Add circle navigation

--- slider-ds.js
+++ slider-ds.js
@@ -2,27 +2,86 @@ const SliderDS = () => {
 
     const slider = document.getElementById('slider-ds');
     const slides = slider.getElementsByTagName('img');
-    const prevBtn = slider.querySelector('#prev-btn');
-    const nextBtn = slider.querySelector('#next-btn');
+    const prevBtn = createPrevBtn();
+    const nextBtn = createNextBtn();
+    const circles = createCircles();
 
     let counter = 0;
 
     // By default, show the first slide
     slides[counter].classList.add('show');
+    circles[counter].classList.add('selected');
+
+    function createPrevBtn() {
+        const btn = document.createElement('i');
+
+        btn.id = 'prev-btn';
+        btn.classList.add('fas');
+        btn.classList.add('fa-arrow-circle-left');
+        btn.classList.add('fa-2x');
+
+        slider.appendChild(btn);
+
+        return slider.querySelector('#prev-btn');;
+    };
+
+    function createNextBtn() {
+        const btn = document.createElement('i');
+
+        btn.id = 'next-btn';
+        btn.classList.add('fas');
+        btn.classList.add('fa-arrow-circle-right');
+        btn.classList.add('fa-2x');
+
+        slider.appendChild(btn);
+
+        return slider.querySelector('#next-btn');
+    };
+
+    function createCircles() {
+        const circlesNav = document.createElement('div');
+
+        circlesNav.classList.add('circles-nav');
+
+        for(let i = 0; i < slides.length; i++) {
+            const circle = document.createElement('div');
+            circle.classList.add('circle');
+            circle.dataset.position = i;
+            circlesNav.appendChild(circle);
+        }
+
+        slider.appendChild(circlesNav);
+
+        return slider.querySelector('.circles-nav').children;
+    };
 
     const loadEventListeners = () => {
         prevBtn.addEventListener('click', () => {
             slides[counter].classList.remove('show');
+            circles[counter].classList.remove('selected');
             counter--;
             if(counter < 0) counter = slides.length - 1;
             slides[counter].classList.add('show');
+            circles[counter].classList.add('selected');
         });
 
         nextBtn.addEventListener('click', () => {
             slides[counter].classList.remove('show');
+            circles[counter].classList.remove('selected');
             counter++;
             if(counter >= slides.length) counter = 0;
             slides[counter].classList.add('show');
+            circles[counter].classList.add('selected');
+        });
+
+        Array.from(circles).forEach(circle => {
+            circle.addEventListener('click', () => {
+                slides[counter].classList.remove('show');
+                circles[counter].classList.remove('selected');
+                counter = circle.dataset.position;
+                slides[counter].classList.add('show');
+                circles[counter].classList.add('selected');
+            });
         });
     };
 
