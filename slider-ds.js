4910a26fc3f7d8c5ed27f6b110dc683d8ee357b5
Daniel <danisolo91@gmail.com>
2021-02-23 20:09:50 +0100

    New code (faster and without bugs)

--- slider-ds.js
+++ slider-ds.js
@@ -1,77 +1,38 @@
 const SliderDS = () => {
 
     const slider = document.getElementById('slider-ds');
-    const slidesContainer = slider.querySelector('.slides');
-    const slides = slidesContainer.children;
+    const slides = slider.getElementsByTagName('img');
     const prevBtn = slider.querySelector('#prev-btn');
     const nextBtn = slider.querySelector('#next-btn');
-    const size = slides[0].clientWidth;
-    let counter = 1;
 
-    /*
-    ** slidesContainer is flex display, so when we translateX,
-    ** it will 'drag' with it all the images one image size
-    ** to the left. The initial position in the array is 1,
-    ** because 0 is a clone (last image)
-    */
-    slidesContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    let counter = 0;
 
-    const createClones = () => {
-        const firstSlideClone = slides[0].cloneNode(true);
-        const lastSlideClone = slides[slides.length - 1].cloneNode(true);
-
-        firstSlideClone.id = 'firstSlideClone';
-        lastSlideClone.id = 'lastSlideClone';
-
-        slidesContainer.insertBefore(
-            lastSlideClone,
-            slidesContainer.firstChild
-        );
-
-        slidesContainer.appendChild(firstSlideClone);
-    };
+    // By default, show the first slide
+    slides[counter].classList.add('show');
 
     const loadEventListeners = () => {
         prevBtn.addEventListener('click', () => {
-            if(counter >= slides.length - 1) return;
-            slidesContainer.style.transition = 'transform 0.4s ease-in-out';
+            slides[counter].classList.remove('show');
             counter--;
-            slidesContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+            if(counter < 0) counter = slides.length - 1;
+            slides[counter].classList.add('show');
         });
 
         nextBtn.addEventListener('click', () => {
-            if(counter <= 0) return;
-            slidesContainer.style.transition = 'transform 0.4s ease-in-out';
+            slides[counter].classList.remove('show');
             counter++;
-            slidesContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
-        });
-
-        slidesContainer.addEventListener('transitionend', () => {
-            if(counter < 0) counter = 0;
-            if(counter > slides.length -1) counter = slides.length -1;
-            if(slides[counter].id === 'lastSlideClone') {
-                // jump to the last slide without any transition effect
-                slidesContainer.style.transition = 'none';
-                counter = slides.length - 2;
-                slidesContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
-            }
-            if(slides[counter].id === 'firstSlideClone') {
-                // jump to the first slide without any transition effect
-                slidesContainer.style.transition = 'none';
-                counter = 1;
-                slidesContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
-            }
+            if(counter >= slides.length) counter = 0;
+            slides[counter].classList.add('show');
         });
     };
 
     const init = () => {
-        createClones();
         loadEventListeners();
     };
 
     return { init }
 };
 
-let mySlider = SliderDS();
+const mySlider = SliderDS();
 
 mySlider.init();
